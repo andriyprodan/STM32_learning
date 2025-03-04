22583e946aa7ae35900a1ec53b4b9fa0257c54de
andriyprodan <prodandgri@gmail.com>
2025-03-04 15:22:03 +0200

    initial

---
+++ host/000TestProject/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 4, 2025
+ *      Author: proda
+ */
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World\n");
+
+	printf("Press 'Enter' to exit the program\n");
+
+	getchar();
+
+	return 0;
+}
